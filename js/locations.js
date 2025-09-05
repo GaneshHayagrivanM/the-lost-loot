76eaeefde8a9b0159161be2f244a7a4af709848a
Ganesh Hayagrivan M <72701140+GaneshHayagrivanM@users.noreply.github.com>
2025-09-05 21:53:55 +0530

    Change coordinates for checkpoint ID 1

--- js/locations.js
+++ js/locations.js
@@ -4,7 +4,7 @@
 // The key is the checkpoint ID, and the value is an object with latitude and longitude.
 
 export const CHECKPOINT_LOCATIONS = {
-    1: { lat: 48.8583, lon: 2.2923 }, // Eiffel Tower, Paris
+    1: { lat: 13.053506, lon:80.201909 },
     // Future checkpoints will be added here
     // 2: { lat: 40.7128, lon: -74.0060 }, // Example: New York
     // 3: { lat: 34.0522, lon: -118.2437 }, // Example: Los Angeles
