0152c0e31cddd7f09863f0df33b549a65f027e07
Ganesh Hayagrivan M <72701140+GaneshHayagrivanM@users.noreply.github.com>
2025-09-10 08:53:18 +0530

    Update locations.js

--- js/locations.js
+++ js/locations.js
@@ -5,7 +5,7 @@
 
 export const CHECKPOINT_LOCATIONS = {
     1: { lat: 48.8583, lon: 2.2923 }, // Eiffel Tower, Paris
-    2: { lat: 13.053413, lon: 80.202483 }, // SSN College of Engineering
+    2: { lat: 12.7512664, lon: 80.1964620 }, // SSN College of Engineering
     // Future checkpoints will be added here
     // 3: { lat: 34.0522, lon: -118.2437 }, // Example: Los Angeles
 };
