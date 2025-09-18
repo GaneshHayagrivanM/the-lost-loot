174f6c188301989ec65ea9ea695bf172f1603289
Varshighan <rohithvarshighan2310966@ssn.edu.in>
2025-09-18 15:09:05 +0530

    hi

--- js/locations.js
+++ js/locations.js
@@ -4,8 +4,9 @@
 // The key is the checkpoint ID, and the value is an object with latitude and longitude.
 
 export const CHECKPOINT_LOCATIONS = {
-    1: { lat: 48.8583, lon: 2.2923 }, // Eiffel Tower, Paris
-    2: { lat: 12.7512664, lon: 80.1964620 }, // SSN College of Engineering
+    // 1: { lat: 48.8583, lon: 2.2923 }, // Eiffel Tower, Paris
+    1: { lat: 12.7495076, lon: 80.198654}, // Eiffel Tower, Paris
+    2: { lat: 12.7512664, lon: 80.1964620 }, // SSN College of Engineering(12.7495076, Longitude:80.198654)
     // Future checkpoints will be added here
     // 3: { lat: 34.0522, lon: -118.2437 }, // Example: Los Angeles
 };
