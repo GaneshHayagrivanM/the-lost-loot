58b6d7dc86f49030d0cb915478d18e7916f14d22
Ganesh Hayagrivan M <72701140+GaneshHayagrivanM@users.noreply.github.com>
2025-09-26 00:46:15 +0530

    Fix treasure chest scale attributes

--- js/ar-checkpoint5.js
+++ js/ar-checkpoint5.js
@@ -39,7 +39,7 @@ function setupARScene() {
     treasureChest.setAttribute('src', 'assets/treasure_chest.glb');
 
     treasureChest.setAttribute('position', '0 0 -11');
-    treasureChest.setAttribute('scale', '0.04 0.07, 0.04');
+    treasureChest.setAttribute('scale', '0.07 0.04, 0.04');
     treasureChest.setAttribute("rotation", "0 270 0");
 
     treasureChest.addEventListener('model-loaded', () => {
