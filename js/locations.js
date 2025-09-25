fa772ad06fb028fc5a3b43bdfaa8849eec4fc817
Ganesh Hayagrivan M <72701140+GaneshHayagrivanM@users.noreply.github.com>
2025-09-25 16:32:01 +0530

    Update locations.js

--- js/locations.js
+++ js/locations.js
@@ -4,7 +4,7 @@
 // The key is the checkpoint ID, and the value is an object with latitude and longitude.
 
 export const CHECKPOINT_LOCATIONS = {
-    1: { lat: 48.8583, lon: 2.2923 }, // Eiffel Tower, Paris
+    1: { lat: 12.7512664, lon: 80.1964620 }, // Eiffel Tower, Paris
     2: { lat: 12.7512664, lon: 80.1964620 }, 
     3: { lat: 12.7512664, lon: 80.1964620 },
     4: { lat: 12.7512664, lon: 80.1964620 }// SSN College of Engineering
