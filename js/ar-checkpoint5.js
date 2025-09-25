7c0f1747763f87ca72c4b1f2b2e83c5af228d2a2
Ganesh Hayagrivan M <72701140+GaneshHayagrivanM@users.noreply.github.com>
2025-09-26 00:00:53 +0530

    Update ar-checkpoint5.js

--- js/ar-checkpoint5.js
+++ js/ar-checkpoint5.js
@@ -37,8 +37,8 @@ function setupARScene() {
     const treasureChest = document.createElement('a-gltf-model');
     treasureChest.setAttribute('id', 'treasure-chest-model');
     treasureChest.setAttribute('src', 'assets/treasure_chest.glb');
-    treasureChest.setAttribute('position', '0 -1 -4');
-    treasureChest.setAttribute('scale', '0.01 0.01 0.01');
+    treasureChest.setAttribute('position', '0 -4 -4');
+    treasureChest.setAttribute('scale', '0.05 0.05 0.05');
 
     treasureChest.addEventListener('model-loaded', () => {
         console.log("Treasure chest model loaded.");
