e2822413681dd26255295142e71390b718e8376a
Ganesh Hayagrivan M <72701140+GaneshHayagrivanM@users.noreply.github.com>
2025-09-26 09:38:33 +0530

    Update locations.js

--- js/locations.js
+++ js/locations.js
@@ -6,9 +6,9 @@
 export const CHECKPOINT_LOCATIONS = {
     1: { lat: 12.7492010 , lon: 80.1976204}, // Chemical Dept
     2: { lat: 12.7500401 , lon: 80.1934975}, // Physics & Chemistry Lab backside
-    3: { lat: 12.7528197 , lon: 80.1945003 }, // Sports Complex
+    3: { lat: 12.7526297 , lon: 80.1939890 }, // Sports Complex
     4: { lat: 12.7527520 , lon: 80.1964111 }, // Clock tower
     5: { lat: 12.7516235 , lon: 80.1957938 }// Fountain
     // Future checkpoints will be added here
     // 3: { lat: 34.0522, lon: -118.2437 }, // Example: Los Angeles
-};
+};, 
