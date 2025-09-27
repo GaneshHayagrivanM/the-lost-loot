1c0b0be542aac9aff6beb3e09112d9b8e2df3bea
Ganesh Hayagrivan M <72701140+GaneshHayagrivanM@users.noreply.github.com>
2025-09-27 08:46:21 +0530

    Update locations.js

--- js/locations.js
+++ js/locations.js
@@ -4,11 +4,9 @@
 // The key is the checkpoint ID, and the value is an object with latitude and longitude.
 
 export const CHECKPOINT_LOCATIONS = {
-    1: { lat: 12.7492010 , lon: 80.1976204}, // Chemical Dept
-    2: { lat: 12.7500401 , lon: 80.1934975}, // Physics & Chemistry Lab backside
-    3: { lat: 12.7526297 , lon: 80.1939890 }, // Sports Complex
-    4: { lat: 12.7527520 , lon: 80.1964111 }, // Clock tower
+    1: { lat:12.749327  , lon: 80.198171}, // HCL Healthcare
+    2: { lat: 12.751027 , lon: 80.196333}, // ECE
+    3: { lat: 12.753201 , lon: 80.194687 }, // Canteen
+    4: { lat: 12.753077 , lon: 80.197912 }, // Law School
     5: { lat: 12.7516235 , lon: 80.1957938 }// Fountain
-    // Future checkpoints will be added here
-    // 3: { lat: 34.0522, lon: -118.2437 }, // Example: Los Angeles
 };
