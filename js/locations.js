c4c5d46b40427607311674af3cfd3ce9ef622872
Ganesh Hayagrivan M <72701140+GaneshHayagrivanM@users.noreply.github.com>
2025-09-27 10:04:11 +0530

    Update locations.js

--- js/locations.js
+++ js/locations.js
@@ -7,6 +7,6 @@ export const CHECKPOINT_LOCATIONS = {
     1: { lat:12.749327  , lon: 80.198171}, // HCL Healthcare
     2: { lat: 12.751027 , lon: 80.196333}, // ECE
     3: { lat: 12.753201 , lon: 80.194687 }, // Canteen
-    4: { lat: 12.753077 , lon: 80.197912 }, // Law School
+    4: { lat: 12.752512 , lon: 80.198329 }, // Law School
     5: { lat: 12.7516235 , lon: 80.1957938 }// Fountain
 };
